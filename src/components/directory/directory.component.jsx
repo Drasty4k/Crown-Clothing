b9b3c8ff8845d52078803f1db93b9af754fae1b9
DragosDev <barbuta.dragos12@gmail.com>
2021-06-10 18:44:36 +0300

    updating app with a shop page, collection preview component and collection item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import MenuItem from '../menu-item/menu-item.component'
+import MenuItem from "../menu-item/menu-item.component";
 
-import './directory.styles.scss'
+import "./directory.styles.scss";
 
 class Directory extends React.Component {
   constructor() {
@@ -46,16 +46,15 @@ class Directory extends React.Component {
   }
 
   render() {
-      return (
-        <div className="directory-menu">
-            {
-                this.state.sections.map(({id, ...otherSectionProps}) => (
-                    <MenuItem key={id} {...otherSectionProps}/>
-                ))
-            }
-        </div>
-      )
+    const { sections } = this.state;
+    return (
+      <div className="directory-menu">
+        {sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
-export default Directory
+export default Directory;
