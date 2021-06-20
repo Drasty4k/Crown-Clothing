31ee9c6d5bc4752f022f1d06059c7437adf43be0
DragosDev <barbuta.dragos12@gmail.com>
2021-06-20 13:48:01 +0300

    changed some text

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -39,9 +39,9 @@ const CheckoutPage = ({ cartItems, total }) => (
       <span>TOTAL: ${total}</span>
     </div>
     <div className='test-warning'>
-      *Please use the following test credit carf for payments*
+      *Please use the following test credit card for payments*
       <br />
-      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      4242 4242 4242 4242 - Exp: 07/20 - CVV: 123
     </div>
     <StripeCheckoutButton price={total} />
   </div>
