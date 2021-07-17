4c7cd95163e53d76139f17409c8b6fce34b92e85
DragosDev <barbuta.dragos12@gmail.com>
2021-07-17 12:26:08 +0300

    implemented lazy and suspense to optimize the route components

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { withRouter } from "react-router";
 
 import CollectionItem from "../collection-item/collection-item.component";
 
 import "./collection-preview.styles.scss";
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
   <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <h1
+      className="title"
+      onClick={() => history.push(`${match.path}/${routeName}`)}
+    >
+      {title.toUpperCase()}
+    </h1>
     <div className="preview">
       {items.map(
-        (item, idx) =>
-          idx < 4 && <CollectionItem key={item.id} item={item} />
+        (item, idx) => idx < 4 && <CollectionItem key={item.id} item={item} />
       )}
     </div>
   </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
