59cb355f8af412b7f1d1312f8996e3dc8b618c9e
DragosDev <barbuta.dragos12@gmail.com>
2021-07-17 11:30:30 +0300

    maked the App more mobile friendly

--- client/src/components/custom-button/custom-botton.component.jsx
+++ client/src/components/custom-button/custom-botton.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import './custom-button.styles.scss'
 
 const CustomButton = ({ children, ...props }) => (
-  <div class='custom-button' {...props}>{children}</div>
+  <div className='custom-button' {...props}>{children}</div>
 );
 
 export default CustomButton;
